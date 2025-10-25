09a38f3c6fb9d46844cf798e6de27d3695327ffd
Nico <47644445+nicotsx@users.noreply.github.com>
2025-10-25 10:05:32 +0200

    feat: setup bunup for library bundling (#2)

--- main.ts
+++ main.ts
@@ -1,13 +1,9 @@
-import { Cup } from "./src/cup";
+import { Cup, type CheckParams } from "./src/cup";
+import type { Image } from "./src/types/image";
 
-async function main() {
-  const cup = new Cup();
+const cup: Cup = new Cup();
 
-  const res = await cup.check("ghcr.io", "steveiliop56", "tinyauth", "v4.0", [
-    "ghcr.io/steveiliop56/tinyauth@sha256:9e8fb9d58dc69031af395fd570ada147c6c7b98b29c036971bf69738b73d71b0",
-  ]);
-
-  console.log(res);
+export async function check(params: CheckParams): Promise<Image | null> {
+  return await cup.check(params);
 }
 
-main();
